a0816590dd954ce5be6a63e82a432ff9da654291
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-18 14:41:10 +0200

    Add class active to active img

--- main.js
+++ main.js
@@ -9,29 +9,27 @@ const app = new Vue({
             "./assets/img/Yosemite-03.jpeg",
             "./assets/img/Yosemite-04.jpeg",
         ],
-        position: 'first',
-        circleColor: 'white',
-        active: false,
+        position: 'first active',
     },
 
     methods: {
         showNext() {
             // Shows next picture
             if (this.counter === this.images.length - 1) {
-                return this.counter = 0, this.position = 'first', active = true;
+                return this.counter = 0, this.position = 'first active';
             } else if (this.counter === this.images.length - 2) {
-                return this.counter += 1, this.position = 'last', active = true;
+                return this.counter += 1, this.position = 'last active';
             }
-            return this.counter += 1, this.position = ' ', active = true;
+            return this.counter += 1, this.position = 'active';
         },
         showPrev() {
             // Shows previous picture
             if (this.counter === 0) {
-                return this.counter = this.images.length - 1, this.position = 'last', active = true;
+                return this.counter = this.images.length - 1, this.position = 'last active';
             } else if (this.counter === 1) {
-                return this.counter -= 1, this.position = 'first', active = true;
+                return this.counter -= 1, this.position = 'first active';
             }
-            return this.counter -= 1, this.position = ' first', active = true;
+            return this.counter -= 1, this.position = ' first active';
         },
     }
 })
