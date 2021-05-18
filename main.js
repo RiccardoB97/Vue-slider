92a1bf3285c69e3140e3a387458522abd8314cf0
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-18 14:14:50 +0200

    use v-for to show circles for each image

--- main.js
+++ main.js
@@ -10,26 +10,28 @@ const app = new Vue({
             "./assets/img/Yosemite-04.jpeg",
         ],
         position: 'first',
+        circleColor: 'white',
+        active: false,
     },
 
     methods: {
         showNext() {
             // Shows next picture
             if (this.counter === this.images.length - 1) {
-                return this.counter = 0, this.position = 'first';
+                return this.counter = 0, this.position = 'first', active = true;
             } else if (this.counter === this.images.length - 2) {
-                return this.counter += 1, this.position = 'last';
+                return this.counter += 1, this.position = 'last', active = true;
             }
-            return this.counter += 1, this.position = ' ';
+            return this.counter += 1, this.position = ' ', active = true;
         },
         showPrev() {
             // Shows previous picture
             if (this.counter === 0) {
-                return this.counter = this.images.length - 1, this.position = 'last';
+                return this.counter = this.images.length - 1, this.position = 'last', active = true;
             } else if (this.counter === 1) {
-                return this.counter -= 1, this.position = 'first';
+                return this.counter -= 1, this.position = 'first', active = true;
             }
-            return this.counter -= 1, this.position = ' first';
+            return this.counter -= 1, this.position = ' first', active = true;
         },
     }
 })
