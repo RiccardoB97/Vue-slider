9340d3085a4b3a1187e0f196b5ace75945da9019
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-19 10:51:18 +0200

    Add event listener keyup

--- main.js
+++ main.js
@@ -9,28 +9,37 @@ const app = new Vue({
             "./assets/img/Yosemite-03.jpeg",
             "./assets/img/Yosemite-04.jpeg",
         ],
-        position: 'first active',
+        position: 'first',
     },
 
     methods: {
         showNext() {
             // Shows next picture
             if (this.counter === this.images.length - 1) {
-                return this.counter = 0, this.position = 'first active';
+                return this.counter = 0, this.position = 'first ';
             } else if (this.counter === this.images.length - 2) {
-                return this.counter += 1, this.position = 'last active';
+                return this.counter += 1, this.position = 'last ';
             }
-            return this.counter += 1, this.position = 'active';
+            return this.counter += 1, this.position = '';
         },
         showPrev() {
             // Shows previous picture
             if (this.counter === 0) {
-                return this.counter = this.images.length - 1, this.position = 'last active';
+                return this.counter = this.images.length - 1, this.position = 'last ';
             } else if (this.counter === 1) {
-                return this.counter -= 1, this.position = 'first active';
+                return this.counter -= 1, this.position = 'first ';
             }
-            return this.counter -= 1, this.position = ' first active';
+            return this.counter -= 1, this.position = ' first ';
         },
-
+    },
+    mounted() {
+        document.addEventListener('keyup', function (e) {
+            if (e.key === "ArrowRight") {
+                app.showNext()
+            }
+            if (e.key === "ArrowLeft") {
+                app.showPrev()
+            }
+        })
     }
 })
